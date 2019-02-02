ef0ec92bdcd4dbbe25f66a6cec2974b7a52884bd
khalfan <3mri.kh@gmail.com>
2019-02-02 14:12:13 +0400

    add project folder

---
+++ admin.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html dir="rtl">
+<head>
+	<title></title>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+
+	<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
+<link rel="stylesheet" href="style_css.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+</head>
+<body>
+<center>
+
+
+<img src="logo.png" height="400px" width="300px">
+
+
+<div class="uk-card uk-card-default uk-card-body back uk-width-1-2@m">
+صفحة المُشاركات
+</div>
+
+
+<br>
+
+<div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+
+<form method="post" action="">
+<div class="alert alert-warning" role="alert">
+  
+<span class="head">رقم المشاركة: </span><label style="font-size=99px; color:#cc9900">1</label>
+
+</div>
+    <hr class="uk-divider-icon">
+
+<table>
+<tr>
+    <td class="left"><b>الاسم:</b></td>
+    <td>مشعل البوسعيدي</td>
+</tr>
+
+<tr>
+    <td class="left"><b>رقم الهاتف:</b></td>
+    <td>985474365</td>
+</tr>
+
+
+
+</table>
+
+<audio controls >
+  <source src="audio/xx.mp3" type="audio/ogg">
+</audio>
+
+<hr class="uk-divider-icon">
+
+<span class="head">التقييم </span>
+<div class="uk-margin">
+    <input class="uk-input uk-form-width-medium" type="text" placeholder="الرجاء إدخال قيمة 0-20">
+</div>
+
+    <button class="uk-button uk-button-default"><b> ارسال التقييم</b></button>
+
+</form>
+
+</div>
+
+
+
+<br>
+
+<div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+
+<form method="post" action="">
+
+<span class="head">رقم المشاركة: </span><label style="font-size=99px; color:#cc9900">2</label>
+    <hr class="uk-divider-icon">
+
+<table>
+<tr>
+    <td class="left"><b>الاسم:</b></td>
+    <td>مشعل البوسعيدي</td>
+</tr>
+
+<tr>
+    <td class="left"><b>رقم الهاتف:</b></td>
+    <td>985474365</td>
+</tr>
+
+
+
+</table>
+
+<audio controls >
+  <source src="audio/xx.mp3" type="audio/ogg">
+</audio>
+
+<hr class="uk-divider-icon">
+
+<span class="head">التقييم </span>
+<div class="uk-margin">
+    <input class="uk-input uk-form-width-medium" type="text" placeholder="الرجاء إدخال قيمة 0-20">
+</div>
+
+    <button class="uk-button uk-button-default"><b> ارسال التقييم</b></button>
+
+</form>
+
+</div>
+
+<br>
+
+
+</center>
+
+
+
+
+
+
+
+
+
+ <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+ 
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html dir="rtl">
+<head>
+	<title></title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+
+	<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
+<link rel="stylesheet" href="style_css.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+</head>
+<body>
+<center>
+
+
+<img src="logo.png" height="400px" width="300px"><br>
+
+<div class="uk-card uk-card-default uk-card-body back uk-width-1-2@m">
+صفحة الإدارة
+</div>
+
+<div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+
+
+<h3 class="uk-card-title"><b>الرجاء إدخال بياناتك</b></h3>
+    
+    <form>
+
+
+
+  <div class="uk-margin">
+        <div class="uk-inline">
+            <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: user" ></span>
+            <input class="uk-input" type="text" placeholder="المعرّف الشخصي">
+           
+        </div>
+        <br><br>
+        <div class="uk-inline">
+           
+            <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon:  unlock" ></span>
+            <input class="uk-input" type="text" placeholder="الرقم السري"><br>
+        </div>
+    </div>
+
+
+  <div class="uk-margin" uk-margin>
+     
+      <button class="uk-button uk-button-default"><b>دخول</b></button>
+  </div>
+
+    </form>
+
+
+
+
+
+</div>
+
+
+<br>
+
+
+</center>
+
+
+
+
+
+
+
+
+
+   <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  
+</body>
+</html>
